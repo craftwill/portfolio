998518019e1235660e9bb191b90abeaf4d92a31f
William <43345835+production-jeu@users.noreply.github.com>
2019-02-27 15:58:55 -0500

    Des TITES GOUTTES DE PLUIESgit add .!

--- public/js/P5Sketch.js
+++ public/js/P5Sketch.js
@@ -25,9 +25,40 @@ export class P5Sketch {
                     htmlCanvas.width = window.innerWidth;
                     htmlCanvas.height = window.innerHeight;
                 }
+
+                /*
+                START HERE
+                */
+
+                function Drop(){
+                    this.x = p.random(p.width);
+                    this.y = p.random(-200, 100);
+                    this.yspeed = p.random(4, 10);
+                    this.fall = function(){
+                        this.y += this.yspeed;
+                        if(this.y > p.height)
+                            this.y = p.random(-200, -100);
+                    }
+                    this.show = function(){
+                        p.stroke(138, 43, 226);
+                        p.line(this.x, this.y, this.x, this.y+10);
+                    }
+                }
+
+                this.drops = [];
+                for(var x = 0; x < 100; x++){
+                    this.drops.push(new Drop());
+                }
+
             }
             p.draw = function () {
+                p.background(230, 230, 230);
                 p.ellipse(window.innerWidth / 2, window.innerHeight / 2, 40, 40);
+                for(let x = 0; x < p.drops.length; x++){
+                    let d = p.drops[x];
+                    d.fall();
+                    d.show();
+                }
             }
         }, "sketch1");
     }
