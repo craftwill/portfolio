de48fd96dc709222f3131286fd0858dd214b0c36
william <william-gin1@hotmail.com>
2019-11-01 13:46:53 -0400

    Changements pour ETS

--- public/js/ProjetsManager.js
+++ public/js/ProjetsManager.js
@@ -24,6 +24,7 @@ export class ProjetsManager {
         this.listeProjets = [];
         this.contentDiv = document.querySelector(".content");
         this.visionnementProjet = document.querySelector(".visionnementProjet");
+        this.visionnementImage = document.querySelector(".visionnementImage");
         this.voileNoir = document.querySelector(".voileNoir");
         this.listeDivs = document.querySelector(".conteneurProjets").querySelectorAll(".boiteProjet");
         this.visionnementProjetSortir = this.visionnementProjet.querySelector(".visionnementProjetSortir");
@@ -35,7 +36,11 @@ export class ProjetsManager {
             let div = this.listeDivs[x];
             div.setAttribute("data-id", (x+1));
             div.classList.add("p" + (x+1));
-            div.addEventListener("click", (e)=>{this.ouvrirProjet(e, div);});
+            if(true){ //div.getAttribute("data-reader-type") != "image"
+                div.addEventListener("click", (e)=>{this.ouvrirProjet(e, div);});
+            }else{
+                div.addEventListener("click", (e)=>{this.ouvrirImage(e, div);});
+            }
         }
         this.initialisationProjets();
         // Injection de style css pour arranger un problème d'espacement lié au visionneur de projet en mobile
@@ -85,7 +90,7 @@ export class ProjetsManager {
             this.listeDivs[x].style.pointerEvents = "none";
         }
 
-        // Gérer l'appairiton du contenu dans la page de visionnement
+        // Gérer l'apparition du contenu dans la page de visionnement
         let idProjet = div.getAttribute("data-id");
         let p = this.getProjetParID(idProjet);
         this.visionnementProjet.querySelector("div").innerHTML = p.description;
@@ -132,6 +137,44 @@ export class ProjetsManager {
             imgElm.style.opacity = "1";
             this.listeDivs[x].style.pointerEvents = "all";
         }
+    }
+    ouvrirImage(e, div){
+        console.log("Ouvrir visionneuse image");
+
+        let imageDiv = this.visionnementImage.getElementsByTagName('div')[0];
+
+        this.visionnementImage.style.pointerEvents = "default";
+        imageDiv.style.pointerEvents = "default";
+        imageDiv.style.opacity = '1.0';
+
+        // Ouvrir visuellement
+        this.voileNoir.style.opacity = 0.9;
+        this.voileNoir.style.pointerEvents = "all";
+        document.body.style.overflowY = "hidden";
+        // Règle un problème d'affichage avec le footer qui se place devant le visionneur
+        this.footer.style.zIndex = "-2";
+
+        this.visionnementImage.addEventListener("click", (ev)=>{this.fermerImage(ev)});
+
+    }
+    fermerImage(e, div){
+        console.log("Fermer visionneuse image");
+
+        this.visionnementImage.style.pointerEvents = "none";
+        imageDiv.style.pointerEvents = "none";
+        imageDiv.style.opacity = '0.0';
+
+        // Fermer visuellement
+        this.visionnementProjet.style.right = "-100vw";
+        this.voileNoir.style.opacity = 0;
+        this.voileNoir.style.pointerEvents = "none";
+        document.body.style.overflowY = "auto";
+        
+        // Règle un problème d'affichage avec le footer qui se place devant le visionneur
+        setTimeout(() => {
+            this.footer.style.zIndex = "0";
+        }, 800);
+
     }
     // Retourne un projet par son identifiant
     getProjetParID(id){
@@ -205,7 +248,7 @@ export class ProjetsManager {
                     <h1>Défends ton château!</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.png" alt="">
                     <div>
-                        <p>Voici un jeu que j'ai créé avec Unity dans mes temps libres. Vous avez volé la première copie d'Half-Life 3, mais en faisant cela, vous vous êtes mis toute la terre à dos! Vous décidez donc de vous réfugier dans un château de votre défunt oncle pour défendre votre peau.</p>
+                        <p>Voici un jeu que j'ai créé avec Unity dans mes temps libres. Vous avez volé la première copie d'un jeu tant attendu par les fans, Half-Life 3! Vous vous êtes ainsi mis toute la terre à dos! Vous décidez donc de vous réfugier dans le château de votre défunt oncle afin défendre chèrement votre peau!</p>
                     </div>
                     <img src="images/projets/projet${idCompte+1}/p${idCompte+1}_01.png" alt="">
                     <div>
@@ -223,16 +266,16 @@ export class ProjetsManager {
                     <h1>Deadly Nightmare</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.png" alt="">
                     <div>
-                        <p>Deadly Nightmare est le résultat de mon Travail Pratique dans le cours de programmation d'animation l'en Techniques d'intégration multimédia. Le jeu est fait avec Unity et j'ai programmé la majorité des fonctionnalités tandis que mon collègue s'est occupé de l'architecture du niveau et de la modélisation.</p>
+                        <p>Deadly Nightmare est le résultat d'un Travail Pratique dans le cours de programmation d'animation 2 en Techniques d'intégration multimédia. Le jeu est fait avec Unity et C#. J'ai programmé la majorité des fonctionnalitées et mon collègue s'est occupé de l'architecture du niveau et de la modélisation.</p>
                     </div>
                     <div>
-                        <p>L'idée de faire un tel jeu m'a traversé l'esprit surtout pour tester mes compétences en programmation. Ainsi, j'ai réussi à intégrer environs 11 fusils qui ont chacun leur propre modèle de recul et d'animation de recharge.</p>
+                        <p>Mon but en produisant ce jeu était de voir si j'était capable de programmer une système de fusils dynamiques et amusant. Je crois avoir bien réussi le défi, car quand je joue à Deadly Nightmare, j'ai l'impression de jouer à un Call of Duty en mode Zombie.</p>
                     </div>
                     <div>
-                        <p>J'ai aussi compris comment programmer un système de difficulté pour l'apparition des zombies, l'interaction avec les portes et l'achat d'armes sur les murs et aussi le démembrement des zombies avec le tir des fusils.</p>
+                        <p>J'ai aussi programmé un système de difficulté pour l'apparition des zombies, l'interaction avec les portes, l'achat des armes et aussi le démembrement des zombies.</p>
                     </di>
                     <div>
-                        <p>Bref, ce jeu m'a surtout fait comprendre ce que je pouvais faire sur Unity en 3D à l'aide de la programmation et ce fût une expérience enrichissante pour apprendre de mes erreurs.</p>
+                        <p>Bref, ce jeu m'a surtout fait comprendre ce que je pouvais faire sur Unity en 3D et ce fût une expérience enrichissante pour apprendre de mes erreurs.</p>
                     </div>
                 </div>
             `,
@@ -246,10 +289,10 @@ export class ProjetsManager {
                     <h1>Gladiatorio.fun</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.png" alt="">
                     <div>
-                        <p>Gladiatorio fun est un jeu que j'ai développé dans mes temps libres en Phaser avec Node Js, Socket.io et Express. Le jeu est plus un prototype qu'autre chose, il contient plusieurs bugs et le code est mal écrit. Cependant je suis fier d'avoir réussi à programmer un tel jeu par mes propres moyens.</p>
+                        <p>Gladiatorio est un jeu que j'ai développé dans mes temps libres en Phaser avec NodeJS, Socket.io et Express. Le jeu est plus un prototype qu'autre chose, il contient plusieurs bugs et le code est mal écrit. Cependant je suis fier d'avoir réussi à programmer un tel jeu par mes propres moyens.</p>
                     </div>
                     <div>
-                        <p>C'est également grace à ce projet que j'ai appris à mettre en ligne une application NodeJS à l'aide de Heroku.com. Vous pouvez y jouer ici si vous êtes au moins deux personne: <a href=http://gladiatorio.fun>gladiatorio.fun</a>. Avec les touches 1-2-3 vous pouvez changer d'arme et vous pouvez frapper les autres joueurs avec la souris.</p>
+                        <p>C'est également grace à ce projet que j'ai appris à mettre en ligne une application NodeJS à l'aide d'Herokuapp.com. Vous pouvez y jouer ici si vous êtes au moins deux personne: <a href=http://gladiatorio.fun>gladiatorio.fun</a>. Avec les touches 1-2-3 vous pouvez changer d'arme et vous pouvez frapper les autres joueurs avec la souris.</p>
                     </div>
                 </div>
             `,
@@ -263,7 +306,7 @@ export class ProjetsManager {
                     <h1>Woodie clone</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.png" alt="">
                     <div>
-                        <p>Woodie est un jeu sur l'Apple store et je me suis demandé un jour si je pourrais le refaire en Javascript sans librairies. C'est donc ce que j'ai fait en 2 jours pendant une fin de semaine et j'ai même réussi à le rendre utilisable en version mobile. Le but est de survivre le plus longtemps possible en fesant éclater des colonnes et des rangées de pièces.</p>
+                        <p>Woodie est un jeu sur l'Apple store et je me suis demandé un jour si je pourrais le refaire en Javascript sans librairies. C'est donc ce que j'ai fait durant une fin de semaine. J'ai aussi réussi à le rendre utilisable en version mobile. Le but est de survivre le plus longtemps possible en fesant éclater des colonnes et des rangées de pièces.</p>
                     </div>
                     <div>
                         <p>Vous pouvez y jouer ici: <a href="http://woodie-remake.herokuapp.com/">woodie clone</a></p>
@@ -290,26 +333,26 @@ export class ProjetsManager {
 
                     <img src="images/projets/projet${idCompte+1}/p7_02.png" alt="">
                     <div>
-                        <p>Nous avons donc cherché un plugin externe qui nous permet de simuler une page web directement dans Unity, ce qui nous a ensuite permis de faire fonctionner les 3 mini-jeux dans Unity alors qu'ils même qu'ils sont en Javascript. Nous avons eu beaucoup de problèmes de performances et il a aussi fallu permettre à Unity de faire une communication asynchrone entre le mini-jeu et Unity afin de transmettre des données à celui-ci. </p>
+                        <p>Nous avons donc cherché un plugin externe qui nous permet de simuler une page web directement dans Unity, ce qui nous a ensuite permis de faire fonctionner les 3 mini-jeux dans Unity alors même qu'ils étaient en Javascript. Nous avons eu beaucoup de problèmes de performances et il a aussi fallu permettre à Unity de faire une communication asynchrone avec les mini-jeu afin de transmettre des données. </p>
                     </div>
 
                     <img src="images/projets/projet${idCompte+1}/p7_03.png" alt="">
                     <div>
-                        <p>Bref, ce défi n'a pas été facile, mais j'ai beaucoup appris sur la façon le travail en entreprise en tant que programmeur de jeux et sur mon rôle en tant qu'expert dans mon domaine. Ce jeu est de CREO inc. Vous pouvez présentement le trouver sur l'apple store et sur l'android store en recherchant 'Livré par navire'.</p>
+                        <p>Bref, ce défi n'a pas été facile, mais j'ai beaucoup appris sur le travail en entreprise en tant que programmeur de jeux et sur mon rôle en tant qu'expert dans mon domaine. Ce jeu est de CREO inc. Vous pouvez présentement le trouver sur l'apple store et sur l'android store en recherchant 'Livré par navire'.</p>
                     </div>
                 </div>
             `,
             "apercuProjet"+idCompte+".png"
         ));
         // Projet 08
-        /*this.listeProjets.push(new Projet(
+        this.listeProjets.push(new Projet(
             "Matte painting 01",
             `
                 <div>
                     <h1>Matte painting 01</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.jpg" alt="">
                     <div>
-                        <p>Ils sont les fiers représentants de la lignées des vastayans et ils sont aussi un couple d'amoureux fou, ils se disent souvent qu'ils seront la cause de la mort d'un ou de l'autre, mais les deux s'entendent pour dire qu'ils ne sont pas contre l'idée.</p>
+                        <p>Un matte painting réalisé durant mon premier cours de photoshop à l'aide de pinceaux.</p>
                     </div>
                 </div>
             `,
@@ -323,7 +366,7 @@ export class ProjetsManager {
                     <h1>Matte painting 02</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.jpg" alt="">
                     <div>
-                        <p>Ils sont les fiers représentants de la lignées des vastayans et ils sont aussi un couple d'amoureux fou, ils se disent souvent qu'ils seront la cause de la mort d'un ou de l'autre, mais les deux s'entendent pour dire qu'ils ne sont pas contre l'idée.</p>
+                        <p>Un matte painting réalisé pour le plaisir de créer un simple contraste d'éclairage.</p>
                     </div>
                 </div>
             `,
@@ -337,11 +380,11 @@ export class ProjetsManager {
                     <h1>Concept - Capitalist alien$</h1>
                     <img src="images/projets/apercuProjet${idCompte+1}.png" alt="">
                     <div>
-                        <p>Ils sont les fiers représentants de la lignées des vastayans et ils sont aussi un couple d'amoureux fou, ils se disent souvent qu'ils seront la cause de la mort d'un ou de l'autre, mais les deux s'entendent pour dire qu'ils ne sont pas contre l'idée.</p>
+                        <p>Concept d'un jeu effectué dans mes temps libres. Le joueur doit gérer son entreprise qui commence avec une alien venu de l'espace afin de faire fortune. Il commence son aventure en tant que classique vendeur de limonade, mais qui sait jusqu'où il ira pour continuer de s'enrichir...</p>
                     </div>
                 </div>
             `,
             "apercuProjet"+idCompte+".png"
-        ));*/
+        ));
     }
 }
